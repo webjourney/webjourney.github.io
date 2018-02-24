90f50c8ccea67887dcf90e8fd852e0d60ce46549
webjourney <35312725+webjourney@users.noreply.github.com>
2018-02-23 22:45:12 -0800

    added new gallery page

---
+++ assignments/lesson-7/js/currentdate-7.js
@@ -0,0 +1,29 @@
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday, ";
+weekday[1] = "Monday, ";
+weekday[2] = "Tuesday, ";
+weekday[3] = "Wednesday, ";
+weekday[4] = "Thursday, ";
+weekday[5] = "Friday, ";
+weekday[6] = "Saturday, ";
+    
+var n = weekday[d.getDay()];
+var a = d.getDate();
+var month = new Array(12);
+month[0] = " January ";
+month[1] = " February ";
+month[2] = " March ";
+month[3] = " April ";
+month[4] = " May ";
+month[5] = " June ";
+month[6] = " July ";
+month[7] = " August ";
+month[8] = " September ";
+month[9] = " October ";
+month[10] = " November ";
+month[11] = " December ";
+
+var m = month[d.getMonth()];
+var y = d.getFullYear();
+document.write(n + a + m + y);
---
+++ assignments/lesson-7/js/hamburger-7.js
@@ -0,0 +1,3 @@
+function toggleNavMenu() {
+document.getElementById("myDropdown").classList.toggle("show");
+}
