bcb390429ac404f460d51c820388c5267cea1761
webjourney <35312725+webjourney@users.noreply.github.com>
2018-02-26 12:03:00 -0800

    fix the js file

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,11 +1,12 @@
-/*var high = parsFloat(document.getElementById('highT').innerHTML);
-var low = parseFloat(document.getElementById('lowT').innerHTML);
-var averageT = (high + low) / 2;
-var sp = parseFloat(document.getElementById('windS').innerHTML);
-var f = 35.74 + 0.6215 *  averageT - 35.75 * Math.pow(sp, 0.16) + 0.4275 *  averageT * Math.pow(sp, 0.16);
-*/
-var f =35.74 + 0.6215 *  15 - 35.75 * Math.pow(2, 0.16) + 0.4275 *  2 * Math.pow(3, 0.16)
-document.write(f);
-/*get("windchill").innerHTML= f.toFixed()+ "&deg;F";*/
+let high = parseFloat(document.getElementById('highT').innerHTML);
+let low = parseFloat(document.getElementById('lowT').innerHTML);
+let averageT = (high + low) / 2;
+let sp = parseFloat(document.getElementById('windS').innerHTML);
+let f = 35.74 + 0.6215 *  averageT - 35.75 * Math.pow(sp, 0.16) + 0.4275 *  averageT * Math.pow(sp, 0.16);
 
-/*document.getElementById("windchill").innerHTML= "one two three"/*f + "&deg;F";*/
+//Round the number to two digits after the decimal.
+	var digits = 1;
+	var multiplier = Math.pow(10, digits);
+	result= Math.round(f * multiplier) / multiplier;
+
+document.getElementById('windchill').innerHTML = 'Wind Chill: ' + result;
