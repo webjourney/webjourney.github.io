ce1a0263f1e3387fd25ea7cbe5d8d42949749830
webjourney <35312725+webjourney@users.noreply.github.com>
2018-03-04 22:18:55 -0800

    Storm Center

---
+++ assignments/lesson-8/js/ratingvalue.js
@@ -0,0 +1,13 @@
+function adjustRating(rating) {
+    
+    document.getElementById("ratingvalue").innerHTML = rating;
+
+}
+
+/*var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}*/
