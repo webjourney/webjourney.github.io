053fbfea2241b2ff67440ce2b82732e884daba7d
webjourney <35312725+webjourney@users.noreply.github.com>
2018-03-04 23:37:32 -0800

    fix java and footer

--- assignments/lesson-8/js/ratingvalue.js
+++ assignments/lesson-8/js/ratingvalue.js
@@ -1,20 +1,5 @@
 function adjustRating() {
-var range = document.getElementById('range').value;
-var box = document.getElementById('box').value = range;
+    var rating = document.getElementById('rating').value;
+    document.getElementById('box').innerHTML = rating;
+    
 }
-/*function adjustRating(rating){
-    box.innerHTML=box;
-}
-
-
-
-/*function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}*/
-/*var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}*/
