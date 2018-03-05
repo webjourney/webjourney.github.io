483482c44b1a75d92be5d41e2d2c9c125d4e56ef
webjourney <35312725+webjourney@users.noreply.github.com>
2018-03-04 22:53:52 -0800

    Change 1

--- assignments/lesson-8/js/ratingvalue.js
+++ assignments/lesson-8/js/ratingvalue.js
@@ -1,9 +1,16 @@
-function adjustRating(rating) {
-    
-    document.getElementById("ratingvalue").innerHTML = rating;
-
+function adjustRating() {
+var range = document.getElementById('range').value;
+var box = document.getElementById('box').value = range;
+}
+/*function adjustRating(rating){
+    box.innerHTML=box;
 }
 
+
+
+/*function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}*/
 /*var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
